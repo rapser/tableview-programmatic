f126fecc87d5818f61ac23eadce045d00caa2e87
Miguel Angel Tomairo <migueltomairo@multiplica.com>
2020-01-05 12:47:03 -0500

    Initial Commit

---
+++ tableview-programmatic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableview-programmatic
+//
+//  Created by miguel tomairo on 1/5/20.
+//  Copyright © 2020 rapser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
